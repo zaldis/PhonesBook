21eb682f2d42db1c1ad73a6ce8abcac58d891fbb
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2022-08-14 13:32:01 +0300

    Refactor tests

--- tests/unit/test_contacts.py
+++ tests/unit/test_contacts.py
@@ -1,16 +1,27 @@
 import unittest
-# from unittest.mock import patch
-# from src.utils.contact import filter_contacts_by_field
+import settings
+from src.utils.contact import filter_contacts_by_field
 
 
 class TestContactsFiltration(unittest.TestCase):
-    
-    def test_somethings(self):
-        self.assertTrue(5 != 6, '5 != 6')
 
-    def test_somethings2(self):
-        self.assertFalse(5 == 6, '5 == 6')
+    def setUp(self) -> None:
+        settings.contacts = [
+            {
+                "full_name": 'Anton Bera',
+                "phone_number": "28474927293",
+                "address": "anton street"
+            }
+        ]
+        return super().setUp()
 
-    def test_lists(self):
-        self.assertListEqual([1, 2, 3], [1, 2, 30], 'Lists are not equal')
+    def test_empty_list(self):
+        settings.contacts = []
+
+        found_contacts = filter_contacts_by_field("full_name", "Anton")
+ 
+        self.assertIs(
+            len(found_contacts), 0,
+            'If no contacts are loaded, then no contacts can be found'
+        )
 
